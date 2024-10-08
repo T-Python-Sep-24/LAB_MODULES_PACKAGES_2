aec04828e513c1fc1c186d89da75bd90ee401139
KhuloodGhazi <khulood.gmo@gmail.com>
2024-10-08 19:58:11 +0300

    A Little modification

--- Main.py
+++ Main.py
@@ -6,24 +6,24 @@ add_book(library, "A Little Life", "Hanya Yanagihara", "1447294831")
 add_book(library, "The Secret History", "Tartt Donna", "0140167773")
 add_book(library, "And Then There Were None", "Christie, Agatha", "0062073486")
 
-print("\nAll the books:")
+print("\nAll the Books You Have Added:")
 display_books(library)
 
-print("\nChecking out a book:")
+print("\nChecked Out Books:")
 check_out_book(library, "1447294831")
 check_out_book(library, "0140167773")
 
-print("\nDisplaying all books after checkout:")
+print("\nAll the Books You Have Checkout:")
 display_books(library)
 
-print("\nReturning a book:")
+print("\nThe Returned Books:")
 return_book(library, "1447294831")
 
-print("\nDisplaying all books after return:")
+print("\nAll the Books You Returned:")
 display_books(library)
 
-print("\nRemoving a book:")
+print("\nThe Removing Books:")
 remove_book(library, "1447294831")
 
-print("\nDisplaying all books after removal:")
+print("\nAll The Books After Your Removal:")
 display_books(library)
