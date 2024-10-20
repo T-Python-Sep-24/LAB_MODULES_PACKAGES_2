f42adb770cc17a80ced32d7f2f3974420e8003ab
Alhanouf <hnno3040@gmail.com>
2024-10-20 18:21:38 +0300

    DONE

--- library/librarian.py
+++ library/librarian.py
@@ -9,16 +9,12 @@ def add_book(library, title, author, isbn):
             'available': True
         }
 
-
-
-
 def remove_book(library, isbn):
     if isbn in library:
         del library[isbn]
         print(f"Book with ISBN {isbn} has been removed.")
     else:
         print(f"No book found with ISBN {isbn}.")
-
 
 def return_book(library, isbn):
     if isbn in library:
@@ -27,6 +23,15 @@ def return_book(library, isbn):
     else:
         print(f"No book found with ISBN {isbn}.")        
 
-
 def display_books(library):
-    for isbn, book in library.items():
+    if not library:
+        print("No books in the library.")
+    else:
+        for isbn, book in library.items():
+            availability = "Available" if book['available'] else "Not Available"
+            print(f"ISBN: {isbn}, Title: '{book['title']}', Author: {book['author']}, Status: {availability}")
+
+library = {}
+add_book(library, "1984", "George Orwell", "1234567890")
+add_book(library, "To Kill a Mockingbird", "Harper Lee", "0987654321")
+display_books(library)
